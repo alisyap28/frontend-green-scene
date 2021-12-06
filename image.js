6902eccaa1187a6dd34b21911c7f0a879f2bccc4
alisyap28 <alisyap28@gmail.com>
2021-12-06 21:22:48 +0800

    create/add frontend web app and ajax logic

---
+++ image.js
@@ -0,0 +1,46 @@
+var backendUrl = "http://localhost:5000/check-green-scene";
+
+var button = $("#deteksi");
+var input = $("#gambar");
+var imagePreview = $("#previewgambar");
+var resultContainer = $("#hasil");
+var selectedImage;
+
+function getBase64Image(element) {
+  var file = element.files[0];
+  if (!file) return;
+
+  imagePreview.attr("src", URL.createObjectURL(file));
+  var reader = new FileReader();
+
+  reader.onloadend = function () {
+    selectedImage = reader.result;
+  };
+  reader.readAsDataURL(file);
+}
+
+async function onGetResponseFromBackend(data) {
+  incomingData = await data;
+  parsedData = JSON.parse(incomingData);
+  console.log(parsedData);
+  if (parsedData == 1) {
+    resultContainer.html("<p>hasil: kotor</p>");
+  } else {
+    resultContainer.html("<p>hasil: bersih</p>");
+  }
+}
+
+button.click(function () {
+  var data = JSON.stringify({ imageencoded: selectedImage });
+  resultContainer.html(
+    "<p>loading... mengirim data ke backend & machine learning</p>"
+  );
+
+  $.ajax({
+    url: backendUrl,
+    type: "POST",
+    data: data,
+    contentType: "application/json",
+    complete: onGetResponseFromBackend,
+  });
+});
